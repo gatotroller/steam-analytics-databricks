9c80622d6d0f96d5e8ce5924b4886f12908eb64c
gatotroller <eduardo_jafet31oct@hotmail.com>
2026-03-24 19:57:26 -0600

    feat: Add silver_app_details SCD Type 2

--- src/silver/silver_app_details.py
+++ src/silver/silver_app_details.py
@@ -1,14 +1,75 @@
 # Databricks notebook source
 import pyspark.sql.functions as F
+import pyspark.sql.window as Window
+from delta.tables import DeltaTable
 
 # COMMAND ----------
 df_bronze_app_details = spark.read.table("steam_analytics.bronze.app_details")
+window_app_details = Window.partitionBy("appid").orderBy(F.col("extracted_at_game").desc())
+
+# First execution
+if not spark.catalog.tableExists("steam_analytics.silver.app_details"):
+    df_scd_app_details = (
+        df_bronze_app_details
+        .withColumn("row_num", F.row_number().over(window_app_details))
+        .withColumn("is_current", F.col("row_num") == 1)
+        .withColumn("start_date", F.col("extracted_at_game"))
+        .withColumn(
+            "end_date",
+            F.when(
+                F.col("is_current") == True, None
+            )
+            .otherwise(
+                F.lead("extracted_at_game").over(Window.partitionBy("appid").orderBy("extracted_at_game"))
+            )
+        )
+        .drop("row_num")
+    )
+
+    df_scd_app_details.write.mode("overwrite").saveAsTable("steam_analytics.silver.app_details")
+
+else:
+    df_silver_app_details = spark.read.table("steam_analytics.silver.app_details")
+
+    df_new_app_ids = (df_bronze_app_details
+        .join(df_silver_app_details.filter("is_current = True"), on="appid", how="left_anti")
+    )
+
+    # Or records where data actually changed
+    df_changed = (df_bronze_app_details.alias("new")
+        .join(df_silver_app_details.filter("is_current = True").alias("old"), on="appid")
+        .where(F.col("new.extracted_at_game") != F.col("old.extracted_at_game"))
+        .select("new.*")
+    )
+
+    df_to_merge = df_new_app_ids.unionByName(df_changed)
+
+    silver_table = DeltaTable.forName(spark, "steam_analytics.silver.app_details")
+
+    silver_table.alias("target").merge(
+        df_to_merge
+            .withColumn("is_current", F.lit(True))
+            .withColumn("start_date", F.col("extracted_at_game"))
+            .withColumn("end_date", F.lit(None).cast("timestamp"))
+            .alias("source"),
+        "target.appid = source.appid AND target.is_current = True"
+    ).whenMatchedUpdate(set={
+        "is_current": F.lit(False),
+        "end_date": F.current_timestamp()
+    }).whenNotMatchedInsert(values={
+        "appid": "source.appid",
+        "is_current": F.lit(True),
+        "start_date": "source.extracted_at_game",
+        "end_date": F.lit(None),
+        # ... all other columns
+    }).execute()
+
 
 df_silver_app_details = (
     df_bronze_app_details
     .select(
         "appid",
-        F.col("extracted_at_game").alias("extracted_at"),
+        "extracted_at_game",
         "is_free",
         "short_description",
         "header_image",
@@ -32,6 +93,8 @@ df_silver_app_details = (
 
         # Release Date
         F.col("release_date.coming_soon").alias("coming_soon"),
-        F.col("release_date.date").alias("release_date")
+        F.col("release_date.date").alias("release_date"),
     )
 )
+
+# COMMAND ----------
